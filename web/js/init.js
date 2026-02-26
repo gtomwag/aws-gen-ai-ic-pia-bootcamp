c3c0b849aafd1f381a54f5e9c1a873555f296b21
Jada Berenguer <jada.berenguer@slalom.com>
2026-02-26 10:28:39 -0800

    UI changes

--- web/js/init.js
+++ web/js/init.js
@@ -8,12 +8,15 @@ btnSend.addEventListener('click', sendChat);
 btnDisruption.addEventListener('click', createDisruption);
 btnConfirm.addEventListener('click', confirmSelection);
 btnEscalate.addEventListener('click', escalate);
-btnVoiceAgent.addEventListener('click', startVoiceConversation);
-btnVoiceTab.addEventListener('click', () => {
-  if (!isVoiceCallActive) {
-    showScreen('screenChat');
-  }
-  startVoiceConversation();
-});
 
+// Trip card event listeners
+const btnViewRebookingOptions = document.getElementById('btnViewRebookingOptions');
+const btnCloseRebooking = document.getElementById('btnCloseRebooking');
 
+if (btnViewRebookingOptions) {
+  btnViewRebookingOptions.addEventListener('click', showRebookingOptions);
+}
+
+if (btnCloseRebooking) {
+  btnCloseRebooking.addEventListener('click', hideRebookingOptions);
+}
